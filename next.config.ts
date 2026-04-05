1f4735c9689683db7794b3ce2f9e9030d8698143
hypershad1c <spox618@gmail.com>
2026-04-05 23:40:00 +0100

    fourth commit

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // This allows your mobile device or other network devices 
-    // to see live updates (HMR) without being blocked.
-    allowedDevOrigins: ['192.168.1.99'],
-  },
-  // You can add other standard config options here
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
